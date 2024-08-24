6343970dbb3f579a78eff71c6c9522633bb3113c
Davi <davift@gmail.com>
2024-08-24 15:12:42 -0400

    TorTunnel V1

---
+++ iptables.rules.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# Route via TorTunnel
+## Wipping existing rules
+iptables -F
+iptables -t nat -F
+## NAT
+iptables -t nat -A OUTPUT -m owner --uid-owner debian-tor -j RETURN
+## DNS
+iptables -t nat -A OUTPUT -p udp --dport 53 -j REDIRECT --to-ports 9053
+iptables -t nat -A OUTPUT -p tcp --dport 53 -j REDIRECT --to-ports 9053
+iptables -t nat -A OUTPUT -p udp -m owner --uid-owner debian-tor -m udp --dport 53 -j REDIRECT --to-ports 9053
+## DNS address space mapping 10.192.0.0/10
+iptables -t nat -A OUTPUT -p tcp -d 10.192.0.0/10 -j REDIRECT --to-ports 9040
+iptables -t nat -A OUTPUT -p udp -d 10.192.0.0/10 -j REDIRECT --to-ports 9040
+## Excluded networks
+iptables -t nat -A OUTPUT -d 10.0.0.0/8 -j RETURN
+iptables -t nat -A OUTPUT -d 127.0.0.0/9 -j RETURN
+iptables -t nat -A OUTPUT -d 172.16.0.0/12 -j RETURN
+iptables -t nat -A OUTPUT -d 192.168.0.0/16 -j RETURN
+iptables -t nat -A OUTPUT -d 127.128.0.0/10 -j RETURN
+iptables -A OUTPUT -d 10.0.0.0/8 -j ACCEPT
+iptables -A OUTPUT -d 127.0.0.0/9 -j ACCEPT
+iptables -A OUTPUT -d 172.16.0.0/12 -j ACCEPT
+iptables -A OUTPUT -d 192.168.0.0/16 -j ACCEPT
+iptables -A OUTPUT -d 127.128.0.0/10 -j ACCEPT
+## Route redirect
+iptables -t nat -A OUTPUT -p tcp --syn -j REDIRECT --to-ports 9040
+iptables -t nat -A OUTPUT -p udp -j REDIRECT --to-ports 9040
+iptables -t nat -A OUTPUT -p icmp -j REDIRECT --to-ports 9040
+## Accept established
+iptables -A OUTPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+## Allow only Tor output
+iptables -A OUTPUT -m owner --uid-owner debian-tor -j ACCEPT
+iptables -A OUTPUT -j REJECT
---
+++ tortunnel.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+# https://github.com/davift/TorTunnel
+# Verion 1
+
+# Root
+if [ $(id -u) -ne 0 ];
+then
+    echo "Run as root or use 'sudo'"
+    exit 1
+fi
+
+## Options
+if [ $# -eq 0 ]
+then
+    echo ''
+    echo "Usage: tortunnel [--backup] [--install|--start] [--restore|--stop] [--refresh]"
+    echo ''
+    echo "Options:"
+    echo " --backup             backup the original system's configuration before installation"
+    echo " --install, --start   make changes to the system's configuration and start tunneling"
+    echo " --restore, --stop    restore backup with original system's configuration"
+    echo " --refresh            request Tor to acquire a new connection"
+    echo ''
+    exit 0
+fi
+
+# Install Tor if not present
+(which tor > /dev/null) || (apt update && apt install tor -y)
+# Install Curl if not present
+(which curl > /dev/null) || (apt update && apt install curl -y)
+
+## Selection
+while test $# -gt 0;
+do
+    case "$1" in
+        --backup)
+            # Backup Tor
+            cat /etc/tor/torrc > /etc/tor/torrc.bak
+            # Backup Resolution
+            cat /etc/resolv.conf > /etc/resolv.conf.bak
+            # Backup Routes
+            iptables-save > /etc/iptables.rules.bak
+            # Backup System Parameters
+            sysctl -a > /etc/sysctl.conf.bak
+            echo 'Backup completed!'
+            exit 1
+            ;;
+        --install|--start)
+            # Custom Tor
+            cat torrc > /etc/tor/torrc
+            systemctl stop tor ; sleep 2s ; systemctl start tor
+            # Overwrite Resolution (may not persist across boot)
+            cat resolv.conf > /etc/resolv.conf
+            # Overwrite Routes (will not persist across boot)
+            ./iptables.rules.sh
+            # Overwrite System Parameters (will not persist across boot)
+            ./sysctl.conf.sh > /dev/null
+            # Check Connection
+            sleep 3s
+            echo -n 'PublicIP ' ; curl http://ip.me || echo 'unknown'
+            echo -n 'IsTor? ' ; curl -s https://check.torproject.org/api/ip | jq -r '.IsTor' || echo 'unknown'
+            exit 1
+            ;;
+        --restore|--stop)
+            # Restore System Parameters
+            sysctl -p /etc/sysctl.conf.bak &> /dev/null
+            # Restore Routes
+            iptables -F ; iptables -t nat -F ; iptables-restore < /etc/iptables.rules.bak
+            # Restore Tor
+            cat /etc/tor/torrc.bak > /etc/tor/torrc
+            systemctl stop tor ; sleep 2s ; systemctl start tor
+            # Restore Resolution
+            cat /etc/resolv.conf.bak > /etc/resolv.conf
+            echo 'Restauration completed!'
+            exit 1
+            ;;
+        --refresh)
+            # Restarting Tor
+            systemctl stop tor ; sleep 2s ; systemctl start tor
+            sleep 3s
+            echo -n 'PublicIP ' ; (curl http://ip.me 2> /dev/null) || echo 'unknown'
+            echo -n 'IsTor? ' ; curl -s https://check.torproject.org/api/ip | jq -r '.IsTor' || echo 'unknown'
+            exit 1
+            ;;
+        *)
+            echo 'invalid option'
+            exit 1
+            ;;
+    esac
+done
+exit 0
